0f8e8cf79d583dffd5c8ae77277000b15a775976
Michał Stancelewski <michal@stancelewski.pl>
2022-11-30 01:02:57 +0100

    Added custom serializer with nameConverter

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,17 +3,17 @@
 namespace App\Controller;
 
 use App\DTO\LowestPriceEnquiry;
+use App\Service\Serializer\DTOSerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductsController extends AbstractController
 {
     #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
-    public function lowestPrice(Request $request, int $id, SerializerInterface $serializer): Response
+    public function lowestPrice(Request $request, int $id, DTOSerializer $serializer): Response
     {
         if ($request->headers->has('force_fail')) {
             return new JsonResponse([
@@ -33,18 +33,9 @@ class ProductsController extends AbstractController
         $lowestPriceEnquiry->setPromotionId(3);
         $lowestPriceEnquiry->setPromotionName('Black Friday');
 
-        return new JsonResponse($lowestPriceEnquiry, 200);
+        $responseContent = $serializer->serialize($lowestPriceEnquiry, 'json');
 
-       /* return new JsonResponse([
-            "quantity" => 5,
-            "request_location" => "UK",
-            "voucher_code" => "OU812",
-            "request_date" => "2022-04-04",
-            "product_id" => $id,
-            "price" => 100,
-            "discounted_price" => 50,
-            "promotion_id" => 3,
-            "promotion_name" => "Black Friday"
-        ], 200);*/
+        return new Response($responseContent, 200);
+
     }
 }
