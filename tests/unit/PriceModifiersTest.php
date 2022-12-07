278942b5289aa6a6512d7447c568b09c58c2cbb3
Michał Stancelewski <michal@stancelewski.pl>
2022-12-07 20:43:21 +0100

    Create test: FixedPriceVoucher_returns_a_correctly_modified_price

--- tests/unit/PriceModifiersTest.php
+++ tests/unit/PriceModifiersTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\unit;
 use App\DTO\LowestPriceEnquiry;
 use App\Entity\Promotion;
 use App\Filter\Modifier\DateRangeMultiplier;
+use App\Filter\Modifier\FixedPriceVoucher;
 use App\Tests\ServiceTestCase;
 
 class PriceModifiersTest extends ServiceTestCase
@@ -12,7 +13,6 @@ class PriceModifiersTest extends ServiceTestCase
     /** @test  */
     public function DateRangeMultiplier_returns_a_correctly_modified_price(): void
     {
-        // Given
         $enquiry = new LowestPriceEnquiry();
         $enquiry->setQuantity(5);
         $enquiry->setRequestDate('2022-11-27');
@@ -25,10 +25,28 @@ class PriceModifiersTest extends ServiceTestCase
 
         $dateRangeMultiplier= new DateRangeMultiplier();
 
-        // When
         $modifiedPrice = $dateRangeMultiplier->modify(100, 5, $promotion, $enquiry);
 
-        // Then
         $this->assertEquals(250, $modifiedPrice);
+    }
+
+    /** @test  */
+    public function FixedPriceVoucher_returns_a_correctly_modified_price(): void
+    {
+        $enquiry = new LowestPriceEnquiry();
+        $enquiry->setQuantity(5);
+        $enquiry->setVoucherCode('OU812');
+
+        $promotion = new Promotion();
+        $promotion->setName('Voucher OU812');
+        $promotion->setAdjustment(100);
+        $promotion->setCriteria(["code" => "OU812"]);
+        $promotion->setType('fixed_price_voucher');
+
+        $fixedPriceVoucher = new FixedPriceVoucher();
+
+        $modifiedPrice = $fixedPriceVoucher->modify(150, 5, $promotion, $enquiry);
+
+        $this->assertEquals(500, $modifiedPrice);
     }
 }
