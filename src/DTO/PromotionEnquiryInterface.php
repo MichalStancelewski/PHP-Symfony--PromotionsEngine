b1396eefbc0f785543894d39bb1c6e740fc2d3a5
Michał Stancelewski <michal@stancelewski.pl>
2022-12-12 20:38:35 +0100

    Refactoring some interfaces

--- src/DTO/PromotionEnquiryInterface.php
+++ src/DTO/PromotionEnquiryInterface.php
@@ -2,7 +2,13 @@
 
 namespace App\DTO;
 
+use App\Entity\Product;
+
 interface PromotionEnquiryInterface
 {
+    public function setPromotionId(int $promotionId);
 
+    public function setPromotionName(string $promotionName);
+
+    public function getProduct(): ?Product;
 }
